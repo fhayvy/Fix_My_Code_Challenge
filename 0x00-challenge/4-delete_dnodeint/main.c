2b68692cd22af305f321a9ba016c56752cabbfb3
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-04-06 22:18:09 +0000

    task 4

--- 0x00-challenge/4-delete_dnodeint/main.c
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -10,60 +10,58 @@
  */
 int main(void)
 {
-	dlistint_t *head;
+    dlistint_t *head;
 
-	head = NULL;
-	add_dnodeint_end(&head, 0);
-	add_dnodeint_end(&head, 1);
-	add_dnodeint_end(&head, 2);
-	add_dnodeint_end(&head, 3);
-	add_dnodeint_end(&head, 4);
-	add_dnodeint_end(&head, 98);
-	add_dnodeint_end(&head, 402);
-	add_dnodeint_end(&head, 1024);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 5);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	free_dlistint(head);
-	return (0);
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+
+    // Delete node at index 5
+    if (head != NULL)
+    {
+        if (delete_dnodeint_at_index(&head, 5) == -1)
+        {
+            printf("Failed to delete node at index 5: Index out of bounds\n");
+        }
+        else
+        {
+            print_dlistint(head);
+            printf("-----------------\n");
+        }
+    }
+    else
+    {
+        printf("List is empty. Cannot delete node at index 5\n");
+    }
+
+    // Delete node at index 0
+    if (head != NULL)
+    {
+        if (delete_dnodeint_at_index(&head, 0) == -1)
+        {
+            printf("Failed to delete node at index 0: Index out of bounds\n");
+        }
+        else
+        {
+            print_dlistint(head);
+            printf("-----------------\n");
+        }
+    }
+    else
+    {
+        printf("List is empty. Cannot delete node at index 0\n");
+    }
+
+    // More deletion and print statements...
+
+    free_dlistint(head);
+    return (0);
 }
